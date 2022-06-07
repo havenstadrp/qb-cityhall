b2bbf8411ad85db3be3517f80ecf6dff09e7c301
bedar89 <bedar89@gmail.com>
2022-06-07 11:11:03 +0000

    added multiple options for using city services

--- config.lua
+++ config.lua
@@ -4,14 +4,78 @@ Config.UseTarget = GetConvar('UseTarget', 'false') == 'true' -- Use qb-target in
 
 Config.Cityhalls = {
     { -- Cityhall 1
-        coords = vec3(-554.693, -187.189, 38.280),
-        showBlip = true,
+        coords = vec3(-538.581, -193.470, 38.226),
+        showBlip = false,
         blipData = {
             sprite = 487,
             display = 4,
             scale = 0.65,
             colour = 0,
             title = "City Services"
+        },
+        licenses = {
+            ["id_card"] = {
+                label = "Identiteitskaart",
+                cost = 50,
+            },
+            ["driver_license"] = {
+                label = "Rijbewijs",
+                cost = 50,
+                metadata = "driver"
+            },
+            ["weaponlicense"] = {
+                label = "Wapen licentie",
+                cost = 50,
+                metadata = "weapon"
+            },
+            ["permit"] = {
+                label = "Tijdelijk Rijbewijs",
+                cost = 50,
+                metadata = "driver"
+            },
+        }
+    },
+    { -- Cityhall 2
+        coords = vec3(-533.789, -190.619, 38.226),
+        showBlip = true,
+        blipData = {
+            sprite = 487,
+            display = 4,
+            scale = 0.65,
+            colour = 0,
+            title = "Stadhuis"
+        },
+        licenses = {
+            ["id_card"] = {
+                label = "Identiteitskaart",
+                cost = 50,
+            },
+            ["driver_license"] = {
+                label = "Rijbewijs",
+                cost = 50,
+                metadata = "driver"
+            },
+            ["weaponlicense"] = {
+                label = "Wapen licentie",
+                cost = 50,
+                metadata = "weapon"
+            },
+            ["permit"] = {
+                label = "Tijdelijk Rijbewijs",
+                cost = 50,
+                metadata = "driver"
+            },
+        }
+    },
+    { -- Cityhall 3
+        coords = vector3(-528.944, -187.790, 38.226),
+        showBlip = false,
+        blipData = {
+            sprite = 487,
+            display = 4,
+            scale = 0.65,
+            colour = 0,
+            title = "City Services2"
         },
         licenses = {
             ["id_card"] = {
@@ -59,8 +123,30 @@ Config.DrivingSchools = {
 Config.Peds = {
     -- Cityhall Ped
     {
-        model = 'a_m_m_hasjew_01',
-        coords = vec4(-556.200, -185.991, 37.280, 220.0),
+        model = 'a_m_m_hasjew_01', -- City hall Office 1
+        coords = vector4(-538.084, -195.964, 37.226, 7.556),
+        scenario = 'WORLD_HUMAN_STAND_MOBILE',
+        cityhall = true,
+        zoneOptions = { -- Used for when UseTarget is false
+            length = 5.0,
+            width = 5.0,
+            debugPoly = true
+        }
+    },
+    {
+        model = 'a_m_m_hasjew_01', -- City hall Office 2
+        coords = vector4(-533.078, -193.000, 37.226, 48.97),
+        scenario = 'WORLD_HUMAN_STAND_MOBILE',
+        cityhall = true,
+        zoneOptions = { -- Used for when UseTarget is false
+            length = 5.0,
+            width = 5.0,
+            debugPoly = true
+        }
+    },
+    {
+        model = 'a_m_m_hasjew_01', -- City hall Office 3
+        coords = vector4(-528.184, -190.231, 37.226, 38.52),
         scenario = 'WORLD_HUMAN_STAND_MOBILE',
         cityhall = true,
         zoneOptions = { -- Used for when UseTarget is false
